3570e34fe52bf804795f891099134fc2371903f2
Aleksey Efimov <factorng@yandex.ru>
2020-10-07 14:15:20 +0300

    Updates

---
+++ precache-manifest.63ff83a20faf9a3e64cdb824aeb2bc88.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b954c03bbc50e8f50d84db5bf2f0b39",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6ddfaf65ad879aefc67a",
+    "url": "./static/css/main.734cfb39.chunk.css"
+  },
+  {
+    "revision": "91e3bc92c785a5fdd94a",
+    "url": "./static/js/2.954442a9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.954442a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ddfaf65ad879aefc67a",
+    "url": "./static/js/main.fbaf00e5.chunk.js"
+  },
+  {
+    "revision": "a8ef13021948c1ee7e9c",
+    "url": "./static/js/runtime-main.a5163ae5.js"
+  },
+  {
+    "revision": "09f4068bb74fc78ffbce74f5596d6aa5",
+    "url": "./static/media/Inter-Black.09f4068b.woff2"
+  },
+  {
+    "revision": "e37354838bf8078ac0d296217613533f",
+    "url": "./static/media/Inter-Black.e3735483.woff"
+  },
+  {
+    "revision": "7a8cc7241f766a142e15b2948804e547",
+    "url": "./static/media/Inter-Medium.7a8cc724.woff"
+  },
+  {
+    "revision": "f6cf0a0bc5fce3307e2c426eb14eb752",
+    "url": "./static/media/Inter-Medium.f6cf0a0b.woff2"
+  },
+  {
+    "revision": "4dd66a113d54a7f9a1ae913049610617",
+    "url": "./static/media/Inter-Regular.4dd66a11.woff2"
+  },
+  {
+    "revision": "7c539936c4c8c822b59a1bcc6c08a6ec",
+    "url": "./static/media/Inter-Regular.7c539936.woff"
+  },
+  {
+    "revision": "b2fce4ac45f3d116cd48ec8d068ab723",
+    "url": "./static/media/add_icon.b2fce4ac.svg"
+  },
+  {
+    "revision": "5df32a06fdf69f3ae19600017c36d45a",
+    "url": "./static/media/close_icon.5df32a06.svg"
+  },
+  {
+    "revision": "6bfdb35467f51d3ba1f70f8d4a9c2b59",
+    "url": "./static/media/edit__icon.6bfdb354.svg"
+  },
+  {
+    "revision": "f10ac534982d6809f220b16907c7a1db",
+    "url": "./static/media/info-tool-tip-failure.f10ac534.svg"
+  },
+  {
+    "revision": "e38b395c190826b3bf6d78baff1415a0",
+    "url": "./static/media/info-tool-tip-success.e38b395c.svg"
+  },
+  {
+    "revision": "a8acc871456b1001597672e02c5199ca",
+    "url": "./static/media/like.a8acc871.svg"
+  },
+  {
+    "revision": "c6f110191388042ab958ac5e9b285425",
+    "url": "./static/media/logo.c6f11019.svg"
+  }
+]);
